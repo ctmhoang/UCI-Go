fc2be46ae0676efa39dab833fa2e2d807cf64e29
Cameron Hoang <51996720+ctmhoang@users.noreply.github.com>
2020-09-09 03:35:23 -0400

    Finish course 1

---
+++ Course-1-golang-getting-started/week-4/makejson.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"bufio"
+	"encoding/json"
+	"fmt"
+	"os"
+)
+
+func main() {
+	scanner := bufio.NewScanner(os.Stdin)
+	fmt.Printf("Enter your name: ")
+	scanner.Scan()
+	name := scanner.Text()
+
+	fmt.Print("Enter your address: ")
+	scanner.Scan()
+	addr := scanner.Text()
+
+	var infos  = map[string]string {"Name" : name, "Address" : addr}
+	barray, _ :=json.Marshal(infos)
+	fmt.Printf("%s",barray)
+}
---
+++ Course-1-golang-getting-started/week-4/read.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strings"
+)
+
+func main() {
+	type name struct {
+		fname string
+		lname string
+	}
+	fmt.Println("File name? ")
+	scanner := bufio.NewScanner(os.Stdin)
+	scanner.Scan()
+	file, err := os.Open(scanner.Text())
+	if err != nil{
+		fmt.Fprintf(os.Stderr,"There are no such file like the name you provide")
+	}
+	defer file.Close()
+
+	var data = make([]name,0,10)
+
+	fscanner := bufio.NewScanner(file)
+	for fscanner.Scan(){
+		line := fscanner.Text();
+		info := strings.Split(line," ")
+		data = append(data, name{
+			fname: info[0],
+			lname: info[1],
+		})
+	}
+
+	for _,val := range data{
+		fmt.Println(val)
+	}
+}
